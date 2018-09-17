708890071e9986731c4ff512ecf87cbbab2f64d8
Ivan Svetunkov <ivan@svetunkov.ru>
2018-09-17 14:31:39 +0100

    Initial commit for the package

---
+++ R/hsp.R
@@ -0,0 +1,174 @@
+#' Hurdle Shifted Poisson model
+#'
+#' Construct HSP model on time series data
+#'
+#' HSP model consists of two parts: Simple Exponential Smoothing for demand intervals
+#' and the same method for the mean demand sizes (lambda parameter in Poisson distribution).
+#'
+#' @param data the vector of values (time sereis vector).
+#' @param h forecasting horizon.
+#' @param intervals binary, definign whether to construct prediction intervals or not.
+#' @param level confidence level for prediction intervals.
+#' @param holdout whether to use holdout of h observations or not.
+#'
+#' @return Function returns a model of a class "counter", which contains:
+#' \itemize{
+#' \item model - the name of the constructed model,
+#' \item occurrence - ETS(A,N,N) model for demand intervals (from es() function),
+#' \item sizes - ETS(A,N,N) model for demand sizes (from es() function),
+#' \item fitted - fitted values,
+#' \item forecast - forecasts of the function,
+#' \item lower - lower bound of the prediction interval,
+#' \item upper - upper bound of the prediction interval,
+#' \item actuals - the provided actual values,
+#' \item holdout - the actual values from the holdout (if holdout was set to TRUE),
+#' \item level - confidence level used,
+#' \item accuracy - the error measures for the data if the holdout was TRUE.
+#' }
+#'
+#' @references 
+#' \itemize{
+#' \item Snyder, R. D., Ord, J. K., & Beaumont, A. (2012). Forecasting the
+#' intermittent demand for slow-moving inventories: A modelling approach.
+#' International Journal of Forecasting, 28(2), 485–496.
+#' https://doi.org/10.1016/j.ijforecast.2011.03.009
+#' }
+#' 
+#' @author Ivan Svetunkov, \email{ivan@svetunkov.ru}
+#' 
+#' @keywords ts models
+#' 
+#' @seealso \code{\link[counter]{negbin}, \link[smooth]{es}, \link[smooth]{iss}}
+#'
+#' @examples
+#' y <- c(rpois(50,0.3),rpois(50,0.8))
+#' test <- hsp(y)
+#'
+#' @importFrom smooth es Accuracy
+#' @export hsp
+hsp <- function(data, h=10, intervals=TRUE, level=0.95, holdout=FALSE){
+    
+    # Define obs, the number of observations of in-sample
+    obsInsample <- length(data) - holdout*h;
+    
+    # Define obsAll, the overal number of observations (in-sample + holdout)
+    obsAll <- length(data) + (1 - holdout)*h;
+    
+    # If obsInsample is negative, this means that we can't do anything...
+    if(obsInsample<=0){
+        stop("Not enough observations in sample.",call.=FALSE);
+    }
+    # Define the actual values
+    y <- matrix(data[1:obsInsample],obsInsample,1);
+    datafreq <- frequency(data);
+    
+    # Define occurrence variable
+    ot <- (y!=0)*1;
+    obsNonzero <- sum(ot);
+    
+    # Define non-zero demand
+    yot <- matrix(y[y!=0],obsNonzero,1);
+    
+    #### Fit the model to the demand intervals ####
+    # Define the matrix of demand intervals
+    zeroes <- c(0,which(y!=0));
+    zeroes <- diff(zeroes);
+    # Number of intervals in Croston
+    iyt <- matrix(zeroes,length(zeroes),1);
+    newh <- which(y!=0);
+    newh <- newh[length(newh)];
+    newh <- obsInsample - newh + h;
+    
+    modelOccurrence <- smooth::es(iyt,"ANN",h=newh,silent=TRUE);
+    
+    pFitted <- rep((modelOccurrence$fitted),zeroes);
+    tailNumber <- obsInsample - length(pFitted);
+    if(tailNumber>0){
+        pFitted <- c(pFitted,modelOccurrence$forecast[1:tailNumber]);
+    }
+    pForecast <- modelOccurrence$forecast[(tailNumber+1):newh];
+    
+    pFitted <- ts(1/pFitted,start=start(y),frequency=frequency(y));
+    pForecast <- ts(1/pForecast, start=time(y)[obsInsample]+deltat(y),frequency=frequency(y));
+    states <- 1/modelOccurrence$states;
+    
+    #### Fit the model to the demand sizes ####
+    modelSizes <- smooth::es(yot,"ANN",h=h,silent=TRUE);
+    
+    lambdaFitted <- rep(0,obsInsample);
+    m <- 0;
+    for(i in 1:obsInsample){
+        if(y[i]!=0){
+            m <- m + 1;
+        }
+        
+        if(m==0){
+            lambdaFitted[i] <- modelSizes$fitted[1];
+        }
+        else{
+            lambdaFitted[i] <- modelSizes$fitted[m];
+        }
+    }
+    
+    lambdaForecast <- modelSizes$forecast;
+    
+    yFitted <- lambdaFitted * pFitted;
+    
+    yForecast <- yUpper <- yLower <- rep(NA,h);
+    levels <- level - (1 - pForecast);
+    
+    for(i in 1:h){
+        ySimulated <- rpois(100000,lambdaForecast[i]-1)+1;
+        if(intervals){
+            yUpper[i] <- quantile(ySimulated,(1+levels[i])/2);
+            if(pForecast[i]==1){
+                yLower[i] <- quantile(ySimulated,(1-levels[i])/2);
+            }
+            else{
+                yLower[i] <- 0;
+            }
+        }
+        yForecast[i] <- mean(ySimulated);
+    }
+    
+    yHoldoutStart <- time(data)[obsInsample]+deltat(data);
+    
+    if(holdout){
+        yHoldout <- ts(data[(obsInsample+1):obsAll],start=yHoldoutStart,frequency=datafreq);
+        otHoldout <- yHoldout!=0;
+        errormeasures <- Accuracy(yHoldout,pForecast*yForecast,y,digits=5)
+    }
+    else{
+        yHoldout <- NA;
+        errormeasures <- NA;
+    }
+    
+    yForecast <- ts(pForecast*yForecast,start=yHoldoutStart,frequency=datafreq);
+    yUpper <- ts(yUpper,start=yHoldoutStart,frequency=datafreq);
+    yLower <- ts(yLower,start=yHoldoutStart,frequency=datafreq);
+    yFitted <- ts(yFitted,start=start(y),frequency=datafreq)
+    
+    model <- list(model="HSP",occurrence=modelOccurrence,sizes=modelSizes,
+                  fitted=yFitted,forecast=yForecast,lower=yLower,upper=yUpper,
+                  actuals=data,holdout=yHoldout,accuracy=errormeasures,level=level);
+
+    return(structure(model, class="counter"));
+}
+
+#' @importFrom greybox graphmaker
+plot.counter <- function(x, ...){
+    if(any(!is.na(c(x$lower,x$upper)))){
+        graphmaker(x$actuals, x$forecast, x$fitted, x$lower, x$upper, x$level, ...);
+    }
+    else{
+        graphmaker(x$actuals, x$forecast, x$fitted, ...);
+    }
+}
+
+print.counter <- function(x, ...){
+    cat("The model constructed: "); cat(x$model);
+    if(any(!is.na(x$holdout))){
+        cat("\nAccuracy:\n");
+        print(x$accuracy[c("sMAE","sMSE","sCE","RelMAE","RelMSE","RelAME","sPIS")]);
+    }
+}
---
+++ R/negbin.R
@@ -0,0 +1,157 @@
+#' Dynamic Negative Binomial model
+#'
+#' Construct NegBin model on time series data
+#'
+#' The model constructs Negative Binomial model parameterised over the mean and
+#' dispersion parameter using Simple Exponential Smoothing.
+#'
+#' @param data the vector of values (time sereis vector).
+#' @param h forecasting horizon.
+#' @param intervals binary, definign whether to construct prediction intervals or not.
+#' @param level confidence level for prediction intervals.
+#' @param holdout whether to use holdout of h observations or not.
+#'
+#' @return Function returns a model of a class "counter", which contains:
+#' \itemize{
+#' \item model - the name of the constructed model,
+#' \item occurrence - ETS(A,N,N) model for demand intervals (from es() function),
+#' \item sizes - ETS(A,N,N) model for demand sizes (from es() function),
+#' \item fitted - fitted values,
+#' \item forecast - forecasts of the function,
+#' \item lower - lower bound of the prediction interval,
+#' \item upper - upper bound of the prediction interval,
+#' \item actuals - the provided actual values,
+#' \item holdout - the actual values from the holdout (if holdout was set to TRUE),
+#' \item level - confidence level used,
+#' \item probabilty - the probability of success,
+#' \item dispersion - the dispersion variable,
+#' \item alpha - smoothing parameter of the model,
+#' \item initial - the initial value for the time varying mean,
+#' \item accuracy - the error measures for the data if the holdout was TRUE.
+#' }
+#'
+#' @references 
+#' \itemize{
+#' \item Snyder, R. D., Ord, J. K., & Beaumont, A. (2012). Forecasting the
+#' intermittent demand for slow-moving inventories: A modelling approach.
+#' International Journal of Forecasting, 28(2), 485–496.
+#' https://doi.org/10.1016/j.ijforecast.2011.03.009
+#' }
+#' 
+#' @author Ivan Svetunkov, \email{ivan@svetunkov.ru}
+#' 
+#' @keywords ts models
+#' 
+#' @seealso \code{\link[counter]{hsp}, \link[smooth]{es}, \link[smooth]{iss}}
+#'
+#' @examples
+#' y <- c(rpois(50,0.3),rpois(50,0.8))
+#' test <- negbin(y)
+#'
+#' @importFrom nloptr nloptr
+#' @importFrom stats deltat dnbinom frequency quantile rnbinom rpois start time ts
+#' @export negbin
+negbin <- function(data, h=10, intervals=TRUE, level=0.95, holdout=FALSE){
+    
+    # This function constructs Negative Binomial model proposed in Snyder et al.(2012).
+    #
+    ### Values:
+    # probability - probability of success,
+    # size - target number of successful trials,
+    # alpha - smoothing parameter used in the model,
+    # initial - initial value of the model,
+    #
+    ### Author
+    # Ivan Svetunkov
+    #
+    ### Examples:
+    # y <- c(rpois(50,0.3),rpois(50,0.8))
+    # test <- negbin(y)
+    
+    # Define obs, the number of observations of in-sample
+    obsInsample <- length(data) - holdout*h;
+    
+    # Define obsAll, the overal number of observations (in-sample + holdout)
+    obsAll <- length(data) + (1 - holdout)*h;
+    
+    # If obsInsample is negative, this means that we can't do anything...
+    if(obsInsample<=0){
+        stop("Not enough observations in sample.",call.=FALSE);
+    }
+    # Define the actual values
+    y <- matrix(data[1:obsInsample],obsInsample,1);
+    datafreq <- frequency(data);
+    
+    at <- yFitted <- rep(NA,obsInsample);
+    yForecast <- yUpper <- yLower <- atForecast <- rep(NA,h);
+    
+    muFitter <- function(mu0, alpha){
+        yFitted[1] <- mu0;
+        for(i in 2:obsInsample){
+            yFitted[i] <- alpha * y[i-1] + (1-alpha)*yFitted[i-1];
+        }
+        return(yFitted);
+    }
+    
+    CF <- function(A){
+        yFitted <- muFitter(A[1], A[2]);
+        p <- A[3];
+        at <- yFitted * p / (1-p);
+        
+        CFValue <- -sum(dnbinom(y, at, p, log=TRUE));
+        return(CFValue);
+    }
+    
+    # Initial value, smoothing parameter and probability
+    A <- c(mean(y), 0.1, sum(y!=0)/obsInsample);
+    ALower <- rep(0, 3);
+    AUpper<- c(Inf, 1, 1);
+    
+    res <- nloptr::nloptr(A, CF, lb=ALower, ub=AUpper,
+                          opts=list("algorithm"="NLOPT_LN_BOBYQA", "xtol_rel"=1e-8, "maxeval"=500));
+    
+    A <- res$solution;
+    
+    p <- A[3];
+    yFitted <- muFitter(A[1], A[2]);
+    at <- yFitted * p / (1-p);
+    atForecast[] <- at[length(at)];
+    
+    # for(i in 1:obsInsample){
+    #     ySimulated <- rnbinom(10000,at[i],p);
+    #     yFitted[i] <- mean(ySimulated);
+    # }
+    
+    for(i in 1:h){
+        ySimulated <- rnbinom(10000,atForecast[i],p);
+        yForecast[i] <- mean(ySimulated);
+        if(intervals){
+            yUpper[i] <- quantile(ySimulated,(1+level)/2);
+            yLower[i] <- quantile(ySimulated,(1-level)/2);
+        }
+    }
+    
+    yHoldoutStart <- time(data)[obsInsample]+deltat(data);
+    
+    if(holdout){
+        yHoldout <- ts(data[(obsInsample+1):obsAll],start=yHoldoutStart,frequency=datafreq);
+        errormeasures <- Accuracy(yHoldout,yForecast,y,digits=5)
+    }
+    else{
+        yHoldout <- NA;
+        errormeasures <- NA;
+    }
+    
+    yForecast <- ts(yForecast,start=yHoldoutStart,frequency=datafreq);
+    yUpper <- ts(yUpper,start=yHoldoutStart,frequency=datafreq);
+    yLower <- ts(yLower,start=yHoldoutStart,frequency=datafreq);
+    yFitted <- ts(yFitted,start=start(y),frequency=datafreq);
+    at <- ts(c(at,atForecast),start=start(y),frequency=datafreq);
+    
+    model <- list(model="NegBin", fitted=yFitted,forecast=yForecast,lower=yLower,upper=yUpper,
+                  probabilty=p,dispersion=at,alpha=A[2],initial=A[1],
+                  actuals=data,holdout=yHoldout,accuracy=errormeasures,level=level);
+
+    return(structure(model, class="counter"));
+    
+}
